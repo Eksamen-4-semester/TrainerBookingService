0b071bfbbda7a1e44200d367cee9391899f2e911
Nicl841 <eaa24nkjostudents@gmail.com>
2026-05-08 12:56:06 +0200

    Initial commit - PersonalTrainerService

---
+++ PersonalTrainerService/Controllers/PersonalTrainerController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalTrainerService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonalTrainerController : ControllerBase
+{
+}
